75e43ce75dc811de8f699176f043e684cb1a7ead
Matteo Ruina <matteo.ruina@datadoghq.com>
2023-05-02 17:18:25 +0200

    Fix every_test_custom_ns test

--- examples/every_test_custom_ns/main_test.go
+++ examples/every_test_custom_ns/main_test.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
@@ -28,6 +29,9 @@ import (
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/support/kind"
 )
+
+type NamespaceCtxKey string
+type ClusterCtxKey string
 
 var testenv env.Environment
 
@@ -53,11 +57,14 @@ func TestMain(m *testing.M) {
 			cfg.WithKubeconfigFile(kubeconfig)
 
 			// propagate cluster value
-			return context.WithValue(ctx, "cluster", cluster), nil
+			return context.WithValue(ctx, ClusterCtxKey("cluster"), cluster), nil
 		}).Finish(
 		// Teardown func: delete kind cluster
 		func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
-			cluster := ctx.Value("cluster").(*kind.Cluster) // nil should be tested
+			cluster := ctx.Value(ClusterCtxKey("cluster")).(*kind.Cluster) // nil should be tested
+			if cluster == nil {
+				return ctx, fmt.Errorf("error getting kind cluster from context")
+			}
 			if err := cluster.Destroy(); err != nil {
 				return ctx, err
 			}
@@ -79,7 +86,7 @@ func TestMain(m *testing.M) {
 // so that the deleteNSForTest routine can look it up and delete it.
 func createNSForTest(ctx context.Context, cfg *envconf.Config, t *testing.T, runID string) (context.Context, error) {
 	ns := envconf.RandomName(runID, 10)
-	ctx = context.WithValue(ctx, nsKey(t), ns)
+	ctx = context.WithValue(ctx, GetNamespaceKey(t), ns)
 
 	t.Logf("Creating NS %v for test %v", ns, t.Name())
 	nsObj := v1.Namespace{}
@@ -89,7 +96,7 @@ func createNSForTest(ctx context.Context, cfg *envconf.Config, t *testing.T, run
 
 // DeleteNSForTest looks up the namespace corresponding to the given test and deletes it.
 func deleteNSForTest(ctx context.Context, cfg *envconf.Config, t *testing.T, runID string) (context.Context, error) {
-	ns := fmt.Sprint(ctx.Value(nsKey(t)))
+	ns := fmt.Sprint(ctx.Value(GetNamespaceKey(t)))
 	t.Logf("Deleting NS %v for test %v", ns, t.Name())
 
 	nsObj := v1.Namespace{}
@@ -97,6 +104,13 @@ func deleteNSForTest(ctx context.Context, cfg *envconf.Config, t *testing.T, run
 	return ctx, cfg.Client().Resources().Delete(ctx, &nsObj)
 }
 
-func nsKey(t *testing.T) string {
-	return "NS-for-%v" + t.Name()
+// GetNamespaceKey returns the context key for a given test
+func GetNamespaceKey(t *testing.T) NamespaceCtxKey {
+	// When we pass t.Name() from inside an `assess` step, the name is in the form TestName/Features/Assess
+	if strings.Contains(t.Name(), "/") {
+		return NamespaceCtxKey(strings.Split(t.Name(), "/")[0])
+	}
+
+	// When pass t.Name() from inside a `testenv.BeforeEachTest` function, the name is just TestName
+	return NamespaceCtxKey(t.Name())
 }
