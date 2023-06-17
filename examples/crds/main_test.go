1f44d2d7db6a68b19b0fbd8110e7c57bd7c9eaf8
Harsha Narayana <harshnar@cisco.com>
2023-06-17 21:09:25 +0530

    Bump sigs.k8s.io/controller-runtime from 0.14.6 to 0.15.0

--- examples/crds/main_test.go
+++ examples/crds/main_test.go
@@ -17,8 +17,10 @@ limitations under the License.
 package crds
 
 import (
+	"context"
 	"os"
 	"testing"
+	"time"
 
 	"sigs.k8s.io/e2e-framework/pkg/env"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
@@ -39,8 +41,19 @@ func TestMain(m *testing.M) {
 
 	testEnv.Setup(
 		envfuncs.CreateKindCluster(kindClusterName),
-		envfuncs.CreateNamespace(namespace),
 		envfuncs.SetupCRDs("./testdata/crds", "*"),
+		envfuncs.CreateNamespace(namespace),
+		func(ctx context.Context, c *envconf.Config) (context.Context, error) {
+			// I have notiecd a lot timing issue with this tests where the actual assessment fails becuase
+			// unable to retrieve the complete list of server APIs: stable.example.com/v1: the server could not find the requested resource
+			// But when you manually check, the resource exists. Just that the tests are running a bit too quick and API server has not
+			// yet started serving the request it seems like. Since there is no clear way today for us to use wait condition directly to
+			// wait for the CRD to be showing up in the list of resource being served by API server, just adding a delay here seem to take
+			// care of the problem.
+			// TODO: add a wait.For conditional helper that can check and wait for the existence of a CRD resource
+			time.Sleep(2 * time.Second)
+			return ctx, nil
+		},
 	)
 
 	testEnv.Finish(
