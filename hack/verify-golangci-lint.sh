9d99c16ff8eaa16280d1097c0eda5d89a522eeba
cpanato <ctadeu@gmail.com>
2025-01-15 09:55:01 +0100

    bump golanci-lint to v1.63.4
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- hack/verify-golangci-lint.sh
+++ hack/verify-golangci-lint.sh
@@ -18,7 +18,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-VERSION=v1.58.0
+VERSION=v1.63.4
 URL_BASE=https://raw.githubusercontent.com/golangci/golangci-lint
 URL=$URL_BASE/$VERSION/install.sh
 
